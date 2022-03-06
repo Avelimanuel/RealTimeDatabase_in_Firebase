3b92a3aad75e3998766d6a1d1a639b6d278c2465
Avelimanuel <aveliemmanuel@gmail.com>
2022-03-06 17:19:23 +0300

    RealTimedatabase

---
+++ app/src/main/java/com/example/realtimedatabase/Employee.kt
@@ -0,0 +1,17 @@
+package com.example.realtimedatabase
+
+class Employee {
+    var fname = ""
+    var empEmail = ""
+    var empIdnum = ""
+    var empPhoneNum = ""
+
+    constructor(Fname:String,EmpMail:String,EmpIdNum:String,EmpPhoneNum:String){
+        this.fname = Fname
+        this.empEmail = EmpMail
+        this.empIdnum = EmpIdNum
+        this.empPhoneNum = EmpPhoneNum
+    }
+
+
+}
